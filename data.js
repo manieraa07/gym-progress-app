b700b4dba88a9507caa060a6ba5293b6398b141d
manieraa07 <manieraa07@gmail.com>
2026-08-07 23:55:22 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,33 +1,24 @@
-export const workoutPlan = [
-  { 
-    id: 'lat_pull', 
-    name: '1. Lat Pulldown (Rozbieżna)', 
-    type: 'together', 
-    targetReps: [8, 10],
-    gifUrl: 'https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExa3gzZW12bW5nb2ZjYWhhZ2lkdjVnZ2wxM2g4ZnVtMzI5Nmh5anZ2ZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/JZeYUA1uJCzXcR30IS/giphy.gif' 
-  },
-  { 
-    id: 'rowing', 
-    name: '2. Wiosłowanie na Maszynie', 
-    type: 'together', 
-    targetReps: [8, 10],
-    gifUrl: '' 
-  },
-  { 
-    id: 'split_section', 
-    type: 'split',
-    martin: [
-      { id: 'm_incline', name: '3M. Ławka Skośna', targetReps: [6, 8], gifUrl: '' },
-      { id: 'm_shoulders', name: '4M. Wyciskanie na Barki', targetReps: [8, 10], gifUrl: '' },
-      { id: 'm_flyes', name: '5M. Rozpiętki', targetReps: [10, 12], gifUrl: '' }
-    ],
-    ana: [
-      { id: 'a_legpress', name: '3A. Suwnica Siedząca', targetReps: [8, 10], gifUrl: '' },
-      { id: 'a_hamstrings', name: '4A. Uginanie Nóg (Dwugłowe)', targetReps: [10, 12], gifUrl: '' },
-      { id: 'a_abductors', name: '5A. Abduktory (Pośladki)', targetReps: [12, 15], gifUrl: '' }
-    ]
-  },
-  { id: 'triceps', name: '6. Triceps', type: 'together', targetReps: [10, 12], gifUrl: '' },
-  { id: 'biceps', name: '7. Biceps', type: 'together', targetReps: [10, 12], gifUrl: '' },
-  { id: 'side_raises', name: '8. Wznosy Bokiem', type: 'together', targetReps: [12, 15], gifUrl: '' }
-];
+export const initialData = {
+  martin: [
+    { id: 'm_lat', name: '1. Ściąganie z góry (Rozbieżna)', target: '8-10 powt.', last: '54 kg x 8', img: '' },
+    { id: 'm_row', name: '2. Wiosłowanie w siadzie (Seated Row)', target: '8-10 powt.', last: '54 kg x 7', img: '' },
+    { id: 'm_incline', name: '3. Wyciskanie na skosie dodatnim', target: '6-8 powt.', last: '5 kg x 8', img: '' },
+    { id: 'm_shoulder', name: '4. Shoulder Press na maszynie', target: '8 powt.', last: '14 kg x 8 / Bary: 22 kg x 7-8', img: '' },
+    { id: 'm_pecdec', name: '5. Rozpiętki na Pec-Decu', target: '8-10 powt.', last: '36 kg x 8', img: '' },
+    { id: 'm_side', name: '6. Wznosy bokiem', target: '10-12 powt.', last: '36 kg x 8', img: '' },
+    { id: 'm_overhead', name: '7. Overhead Triceps Extension', target: '8-10 powt.', last: '10 kg x 7 (wyciąg: 18 kg)', img: '' },
+    { id: 'm_triceps', name: '8. Triceps Extension na maszynie', target: '8-10 powt.', last: '12 kg x 11 / 14 kg x 6', img: '' },
+    { id: 'm_biceps', name: '9. Biceps na modlitewniku (Maszyna / Hantla)', target: '10-12 powt.', last: '5 kg x 20 (maszyna: 32 kg x 9)', img: '' }
+  ],
+  ana: [
+    { id: 'a_lat', name: '1. Ściąganie z góry (Rozbieżna)', target: '8-10 powt.', last: '50 kg x 11', img: '' },
+    { id: 'a_row', name: '2. Wiosłowanie w siadzie (Seated Row)', target: '8-10 powt.', last: '50 kg x 6-7', img: '' },
+    { id: 'a_legpress', name: '3. Suwnica / Beinpresse', target: '8-10 powt.', last: '72 kg x 8', img: '' },
+    { id: 'a_hamstrings', name: '4. Uginanie nóg (Dwugłowy)', target: '10-12 powt.', last: '27 kg', img: '' },
+    { id: 'a_abductor', name: '5. Abduktor (Pośladki)', target: '12 powt.', last: '45 kg x 12', img: '' },
+    { id: 'a_calves', name: '6. Łydki', target: '12-15 powt.', last: '20 kg', img: '' },
+    { id: 'a_overhead', name: '7. Overhead Triceps Extension', target: '8-10 powt.', last: '12 kg x 10', img: '' },
+    { id: 'a_triceps', name: '8. Triceps na maszynie', target: '8-10 powt.', last: '27 kg (pushdown: 17 kg)', img: '' },
+    { id: 'a_biceps', name: '9. Biceps na maszynie', target: '10-12 powt.', last: '18 kg', img: '' }
+  ]
+};
